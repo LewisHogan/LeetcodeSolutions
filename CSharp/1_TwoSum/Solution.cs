391b2a231affa91a73d80d7018d524c61d042afb
Lewis Hogan <lewishogan@live.com>
2022-05-27 00:06:09 +0200

    Add 1 "TwoSum" C# solution

---
+++ CSharp/1_TwoSum/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace _1_TwoSum;
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        for (var firstNumberIndex = 0; firstNumberIndex < nums.Length - 1; firstNumberIndex++)
+        {
+            for (var secondNumberIndex = firstNumberIndex + 1; secondNumberIndex < nums.Length; secondNumberIndex++)
+            {
+                if (nums[firstNumberIndex] + nums[secondNumberIndex] == target) return new[] { firstNumberIndex, secondNumberIndex };
+            }
+        }
+        
+        // Input constraints confirm that we should never get here
+        return new int[] {};
+    }
+}
