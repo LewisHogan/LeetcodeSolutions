2e8b91d75b000a5961f3350be0cb993c3773a01c
Lewis Hogan <lewishogan@live.com>
2022-05-26 21:44:47 +0200

    Add 217 "Contains Duplicate" C# solution

---
+++ CSharp/217_ContainsDuplicate/Solution.cs
@@ -0,0 +1,13 @@
+﻿namespace _217_ContainsDuplicate;
+public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        var encounteredNumbers = new HashSet<int>();
+        foreach (var number in nums) {
+            if (!encounteredNumbers.Add(number)) return true;
+        }
+
+        return false;
+    }
+}
