a99489ee4fd7b88e6fdbde75d62e5e999c4a4aad
Lewis Hogan <lewishogan@live.com>
2022-05-27 23:49:10 +0200

    Add 125 "Valid Palindrome" C# solution

---
+++ CSharp/125_ValidPalindrome/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace _125_ValidPalindrome;
+public class Solution
+{
+    public bool IsPalindrome(string s)
+    {
+        var sanitized = s.ToLower().ToCharArray().Where(c => char.IsLetterOrDigit(c)).ToArray();
+
+        for (var startIndex = 0; startIndex < s.Length / 2; startIndex++)
+        {
+            if (sanitized[startIndex] != sanitized[s.Length - startIndex - 1]) return false;
+        }
+
+        return true;
+    }
+}
