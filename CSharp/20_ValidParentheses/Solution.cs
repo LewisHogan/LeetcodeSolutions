bf32900358a58c93068db676446436dbc0aab1d6
Lewis Hogan <lewishogan@live.com>
2022-05-28 14:28:49 +0200

    Add 20 "Valid Parentheses" C# solution

---
+++ CSharp/20_ValidParentheses/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace _20_ValidParentheses;
+public class Solution
+{
+    private char? GetMatchingBracket(char c)
+    {
+        return c switch
+        {
+            ')' => '(',
+            ']' => '[',
+            '}' => '{',
+            _ => null // Unreachable with input constraints
+        };
+    }
+
+    public bool IsValid(string s) {
+        var stack = new Stack<char>();
+        
+        foreach (var c in s)
+        {
+            if (c == '(' || c == '{' || c == '[')
+            {
+                stack.Push(c);
+            }
+            else
+            {
+                if (stack.Count() == 0) return false;
+
+                var precedingBracket = stack.Pop();
+                if (precedingBracket != GetMatchingBracket(c)) return false;
+            }
+        }
+
+        return stack.Count() == 0;
+    }
+}
