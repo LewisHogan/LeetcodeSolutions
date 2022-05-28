67910528bba57249984106a8ef26a31048f7af3e
Lewis Hogan <lewishogan@live.com>
2022-05-28 13:39:35 +0200

    Add 121 "Best Time to Buy and Sell Stock" C# solution

---
+++ CSharp/121_BestTimeToBuyAndSellStock/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace _121_BestTimeToBuyAndSellStock;
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        var bestProfit = 0;
+        
+        var buyDay = 0;
+        var sellDay = 0;
+
+        while (sellDay < prices.Length)
+        {
+            if (prices[buyDay] > prices[sellDay])
+            {
+                buyDay = sellDay;
+            }
+            else
+            {
+                var profit = prices[sellDay] - prices[buyDay];
+                if (profit > bestProfit)
+                {
+                    bestProfit = profit;
+                }
+            }
+
+            sellDay++;
+        }
+
+        return bestProfit;
+    }
+}
