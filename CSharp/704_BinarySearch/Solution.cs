de497a8eece6fc29fb4f29a133bbc657b58f6455
Lewis Hogan <lewishogan@live.com>
2022-05-29 18:08:59 +0200

    Add 704 "Binary Search" C# solution

---
+++ CSharp/704_BinarySearch/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace _704_BinarySearch;
+public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        var lowIndex = 0;
+        var highIndex = 0;
+
+        while (lowIndex <= highIndex)
+        {
+            var centerIndex = (lowIndex + highIndex) / 2;
+            if (nums[centerIndex] == target) return centerIndex;
+
+            if (nums[centerIndex] < target) lowIndex = centerIndex + 1;
+            else highIndex = centerIndex - 1;
+        }
+
+        return -1;
+    }
+}
