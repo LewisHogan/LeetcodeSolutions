b69b3b2e036b9b09020461480663a49ed75d1f65
Lewis Hogan <lewishogan@live.com>
2022-05-31 19:55:46 +0200

    Add 226 "Invert Binary Tree" C# solution

---
+++ CSharp/226_InvertBinaryTree/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace _226_InvertBinaryTree;
+public class Solution
+{
+    public TreeNode InvertTree(TreeNode root)
+    {
+        if (root == null) return null;
+
+        var tempNode = root.left;
+        root.left = root.right;
+        root.right = tempNode;
+
+        if (root.left != null)
+        {
+            InvertTree(root.left);
+        }
+
+        if (root.right != null)
+        {
+            InvertTree(root.right);
+        }
+
+        return root;
+    }
+}
