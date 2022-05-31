4360267638ee17427d973e93a48fce4d948420e6
Lewis Hogan <lewishogan@live.com>
2022-05-31 20:01:43 +0200

    Add 104 "Max Depth of Binary Tree" C# solution

---
+++ CSharp/104_MaxDepthOfBinaryTree/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace _104_MaxDepthOfBinaryTree;
+public class Solution
+{
+    public int MaxDepth(TreeNode root)
+    {
+        if (root == null) return 0;
+
+        return 1 + Math.Max(MaxDepth(root.left), MaxDepth(root.right));
+    }
+}
