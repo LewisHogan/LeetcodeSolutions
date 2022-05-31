f3bdb4117b58e3655f4875f507e37e16ffd59c8e
Lewis Hogan <lewishogan@live.com>
2022-05-31 19:35:53 +0200

    Add 141 "Linked List Cycle" C# solution

---
+++ CSharp/141_LinkedListCycle/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace _141_LinkedListCycle;
+public class Solution
+{
+    public bool HasCycle(ListNode head)
+    {
+        ListNode currentNode = head;
+
+        HashSet<ListNode> checkedNodes = new HashSet<ListNode>();
+
+        while (currentNode != null)
+        {
+            if (checkedNodes.Contains(currentNode)) return true;
+
+            checkedNodes.Add(currentNode);
+            currentNode = currentNode.next;
+        }
+
+        return false;
+    }
+}
